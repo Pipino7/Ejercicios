b236fb17eb21a05a06e2815f334acc6572af03f6
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-03-31 22:25:40 -0300

    ejercicios

---
+++ buscarPokemonPorId.js
@@ -0,0 +1,28 @@
+// IMPORTANT: Obtener un pokémon por el id
+export const obtenerPokemon = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const pokemon = await Pokemon.findById(id);
+        
+        if (!pokemon) {
+            return res.status(404).json({
+                message: "Pokémon no encontrado",
+                data: null
+            });
+        }
+
+        if (pokemon.deleted) {
+            return res.status(404).json({
+                message: "Pokémon no encontrado",
+                data: null
+            });
+        }
+
+        res.status(200).json({
+            message: "Pokémon encontrado!",
+            data: pokemon
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
